dee5083342fea8d11c6b3c0e7d0ceab37b4abffe
Stig Nervik <stig.nervik@carrier.com>
2024-02-02 15:32:44 +0100

    Added support for send and fetch grpc messages.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -10,6 +10,83 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <!--<HelloWorld msg="You did it!" /> -->
 
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #213305;
+  z-index: 1000;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+
+.wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+  box-sizing: border-box;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+}
+
+nav a {
+  display: inline-block;
+  padding: 1rem;
+  text-decoration: none;
+  color: black;
+}
+
+nav a.router-link-exact-active, nav a.router-link-active {
+  color: #42b983; /* Example active link color */
+}
+
+.router-view {
+  margin-top: 60px; /* Adjust based on header height */
+}
+
+@media (min-width: 1024px) {
+  nav {
+    justify-content: start;
+  }
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+</style>
+
+
+<!-- <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    
+    <div class="wrapper">
+    
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -82,4 +159,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
-<script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <H1> gRPC </H1>
+    <h1> gRPC </h1>
+    <button @click="createUnit"> Create unit </button>
   </main>
 </template>
+
+
+<script setup lang="ts">
+  import { defineComponent, inject, onMounted } from "vue";
+  import { createPromiseClient } from "@connectrpc/connect";
+  import type { PromiseClient, Transport } from "@connectrpc/connect";
+  import { Unit } from "../gen/unit_connect";
+  import { CreateUnitRequest, CreateUnitReply } from "../gen/unit_pb";
+  // import { transportKey} from "../keys";
+import { createGrpcWebTransport } from "@connectrpc/connect-web";
+
+  interface UnitData {
+    client: PromiseClient<typeof Unit> | undefined;
+  }
+
+  // const transport = inject<Transport>(transportKey);
+
+  async function createUnit() {
+    console.log("create unit...");
+    try {
+      const transport = createGrpcWebTransport({ baseUrl: 'http://127.0.0.1:50051',});
+      const client = createPromiseClient(Unit, transport);
+      const request = new CreateUnitRequest();
+      request.name = "Unit 1";
+      request.unitClass = "Input";
+      request.unitFunction = "Input";
+      request.functionBlock = "Input";
+
+      const response = await client.createUnit(request);
+      console.log("Response from server: {}", response)
+
+      } catch (e) {
+        console.log(e);
+    }
+  }
+
+</script>
